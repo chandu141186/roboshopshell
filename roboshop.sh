c5e8c21528777d44b5afef33c4e2a3f4ef677219
chandu141186 <chandu141186@gmail.com>
2025-02-23 15:25:36 +0530

    Ansibleproject

---
+++ roboshop.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+                       
+AMI=ami-0b4f379183e5706b9     
+SG_ID=sg-097a47c9ab00911a2
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web" )
+ZONE_ID=Z070118421CJ28HDS2FD5
+DOMAIN_NAME=chandudevops.shop
+
+for i in "${INSTANCES[@]}"
+do
+  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+  then 
+     INSTANCE_TYPE="t3.small"
+  else 
+     INSTANCE_TYPE="t2.micro"
+  fi
+    IP_ADDRESS=$(aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-04e8a790706df00aa --tag-specifications "ResourceType=instance,Tags=[{Key= Name,Value= $i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+      echo "$i: $IP_ADDRESS"
+
+  #if [ $i == "web" ]
+  #then
+   # IP_ADDRESS="Public IPv4 address"
+  #else
+   # IP_ADDRESS="Private IPv4 addresses"
+
+  #fi  
+     
+   aws route53 change-resource-record-sets 
+   --hosted-zone-id $ZONE_ID 
+   --change-batch '
+  {
+    "Comment": "Creating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$i'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP_ADDRESS'"
+        }]
+      }
+    }]
+  }
+    '
+ done
